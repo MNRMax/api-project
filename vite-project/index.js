4d68678d7cc35df7d211111d816e639e1ae492f2
MNRMax <maximilian.shabatayev@gmail.com>
2022-11-17 10:15:00 -0500

    bvgcx

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,20 +1,6 @@
-const nameElement = document.getElementById("name")
-const ign = document.getElementById("ign")
-const submit = document.getElementById("submit")
-const image = document.getElementById("image")
+import * as Hypixel from "hypixel"
+const client = new Hypixel({ key: '3a8f73f9-b39a-493d-8d5e-44698b00af8a' });
 
-submit.addEventListener
-
-function displaySkin(name) {
-    console.log('test')
-    let rawData = get(`https://api.ashcon.app/mojang/v2/user/${name}`)
-    let data = JSON.parse(rawData)
-    console.log()
-    image.setAttribute('src', `https://crafatar.com/avatars/${data.uuid}?overlay&default=MHF_SAlex`)
-}
-function get(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
+client.getFriends("MNRMax", (err, friends) => {
+    console.log(friends)
+ });    
